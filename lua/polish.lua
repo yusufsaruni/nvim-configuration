5fdecd54ed9ac287fb618bd49d4ca69511fee6d8
yusufsaruni <137294486+yusufsaruni@users.noreply.github.com>
2025-09-24 07:12:45 +0300

    add status line

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,8 +1,15 @@
 
 -- lua/polish.lua
-print("Custom keymaps loaded!")
 vim.opt.mouse = ""
+vim.opt.guicursor = {
+  "n-v-c:block",       -- normal/visual/command = block
+  "i-ci-ve:block",     -- insert/command-line/visual-exclude = block
+  "r-cr:hor20",        -- replace modes = horizontal bar (20%)
+  "o:hor50",           -- operator-pending mode = horizontal bar (50%)
+  "sm:block-blinkwait175-blinkoff150-blinkon175", -- showmatch = block with blink
+}
 
+vim.keymap.set("v", "jk", "<Esc>", { noremap = true, silent = true, desc = "Exit visual mode with Shift+Enter" })
 -- Just set them directly
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true, desc = "Exit insert mode with jj" })
 vim.keymap.set("i", "<S-CR>", "<Esc>", { noremap = true, silent = true, desc = "Exit insert mode with Shift+Enter" })
